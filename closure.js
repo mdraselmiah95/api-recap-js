4511e4b6e86517c74764d413d9bfb411a5ea4d76
rasel <russelroy15@gmail.com>
2021-09-03 18:13:18 +0600

    new closure

--- closure.js
+++ closure.js
@@ -1,4 +1,4 @@
-const bank = (owner) => {
+/* const bank = (owner) => {
   balance = 0;
   return (amount) => {
     // balance = balance + amount;
@@ -6,11 +6,34 @@ const bank = (owner) => {
     return balance;
   };
 };
+ */
+
+const bank = (owner) => {
+  balance = 0;
+  return {
+    deposit: (amount) => {
+      balance += amount;
+      return balance;
+    },
+    withdraw: (amount) => {
+      balance -= amount;
+      return balance;
+    },
+  };
+};
+
+// const cityBank = bank("city");
+// // cityBank(100);
+// console.log(cityBank(100));
+// console.log(cityBank(100));
+// console.log(cityBank(150));
+// console.log(cityBank.balance);
+// console.log(cityBank(190));
 
 const cityBank = bank("city");
-// cityBank(100);
-console.log(cityBank(100));
-console.log(cityBank(100));
-console.log(cityBank(150));
+console.log(cityBank.deposit(100));
+console.log(cityBank.deposit(100));
+console.log(cityBank.deposit(150));
 console.log(cityBank.balance);
-console.log(cityBank(190));
+console.log(cityBank.deposit(190));
+console.log(cityBank.withdraw(100));
